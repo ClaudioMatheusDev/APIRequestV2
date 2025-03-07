00680471b76b20fe43222d4ed72f83956b9be04e
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-03-06 23:25:14 -0300

    🌠 Comencando implementar classe Repository

--- APICatalogo/Repositories/Repository.cs
+++ APICatalogo/Repositories/Repository.cs
@@ -1,6 +1,41 @@
-﻿namespace APICatalogo.Repositories
+﻿using APICatalogo.Context;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repositories
 {
-    public class Class
+    public class Repository<T> : IRepository<T> where T : class
     {
+
+        protected readonly AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? Get(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+        public T Create(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+        public T Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
