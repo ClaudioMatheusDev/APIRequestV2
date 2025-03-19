1569c4b40f30422c85e418fa121ba1d237256df3
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-03-19 17:45:50 -0300

    🌀 Implementando o padrao UnitOfWork

---
+++ APICatalogo/Repositories/IUnityOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace APICatalogo.Repositories
+{
+    public interface IUnityOfWork //interface IUnityOfWork com as propriedades ProdutoRepository e CategoriaRepository e o método Commit
+    {
+        IProdutoRepository ProdutoRepository { get; }//propriedade do tipo IProdutoRepository
+        ICategoriaRepository CategoriaRepository { get; }//propriedade do tipo ICategoriaRepository
+        void Commit(); //salva as alterações no banco de dados
+    }
+}
