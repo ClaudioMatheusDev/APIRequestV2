74c99ed846a4e221146db450614d5fa1bea0b6df
ClaudioMatheusDev <claudiomatheus055@gmail.com>
2025-03-24 18:13:43 -0300

    :neckbeard: : Update CategoriaController

--- APICatalogo/Controllers/CategoriasController.cs
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -9,26 +9,27 @@ namespace APICatalogo.Controllers;
 [ApiController]
 public class CategoriasController : ControllerBase
 {
-    private readonly IRepository<Categoria> _repository;
+    private readonly IUnityOfWork _uof;
     private readonly ILogger<CategoriasController> _logger;
 
-    public CategoriasController(IRepository<Categoria> repository, ILogger<CategoriasController> logger)
+    public CategoriasController(IUnityOfWork uof, ILogger<CategoriasController> logger)
     {
-        _repository = repository;
+
         _logger = logger;
+        _uof = uof;
     }
 
     [HttpGet]
     public ActionResult<IEnumerable<Categoria>> Get()
     {
-        var categorias = _repository.GetAll();//retorna uma lista de objetos do tipo T (Categoria)
+        var categorias = _uof.CategoriaRepository.GetAll();//retorna uma lista de objetos do tipo T (Categoria)
         return Ok(categorias);
     }
 
     [HttpGet("{id:int}", Name = "ObterCategoria")]
     public ActionResult<Categoria> Get(int id)
     {
-        var categoria = _repository.Get(c => c.CategoriaId == id); // c => c.CategoriaId == id é uma expressão lambda que retorna um booleano 
+        var categoria = _uof.CategoriaRepository.Get(c => c.CategoriaId == id); // c => c.CategoriaId == id é uma expressão lambda que retorna um booleano 
 
         if (categoria is null)
         {
@@ -47,7 +48,8 @@ public class CategoriasController : ControllerBase
             return BadRequest("Dados inválidos");
         }
 
-        var categoriaCriada = _repository.Create(categoria);
+        var categoriaCriada = _uof.CategoriaRepository.Create(categoria);//cria a categoria
+        _uof.Commit();//salva as alterações no banco de dados
 
         return new CreatedAtRouteResult("ObterCategoria", new { id = categoriaCriada.CategoriaId }, categoriaCriada);
     }
@@ -61,14 +63,15 @@ public class CategoriasController : ControllerBase
             return BadRequest("Dados inválidos");
         }
 
-        _repository.Update(categoria);
+        _uof.CategoriaRepository.Update(categoria); //atualiza a categoria
+        _uof.Commit();//salva as alterações no banco de dados
         return Ok(categoria);
     }
 
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
-        var categoria = _repository.Get(c=> c.CategoriaId == id); 
+        var categoria = _uof.CategoriaRepository.Get(c=> c.CategoriaId == id); 
 
         if (categoria is null)
         {
@@ -76,7 +79,8 @@ public class CategoriasController : ControllerBase
             return NotFound($"Categoria com id={id} não encontrada...");
         }
 
-        var categoriaExcluida = _repository.Delete(categoria);
+        var categoriaExcluida = _uof.CategoriaRepository.Delete(categoria); //exclui a categoria
+        _uof.Commit();//salva as alterações no banco de dados
         return Ok(categoriaExcluida);
      
     }
