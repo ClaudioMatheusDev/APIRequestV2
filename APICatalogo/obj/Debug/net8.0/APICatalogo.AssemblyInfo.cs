026378db1ddf94a18292b8cc0092efdbbbda62e5
Claudio Matheus <claudio.silva@smartbreeder.com.br>
2025-07-30 13:44:47 -0300

    Atualiza dependências e remove pasta DTOS
    - Removido grupo de itens com a pasta "DTOS" do `APICatalogo.csproj`.
    - Adicionadas referências ao `Pomelo.EntityFrameworkCore.MySql` (v8.0.0-beta.2) e `Swashbuckle.AspNetCore` (v6.5.0).
    - Atualizada a versão informacional da montagem no `APICatalogo.AssemblyInfo.cs`.
    - Alterações em arquivos binários (`APICatalogo.dll`, `APICatalogo.exe`, `APICatalogo.pdb`, `apphost.exe`).
    - Modificações nos arquivos de cache (`APICatalogo.AssemblyInfoInputs.cache` e `APICatalogo.csproj.CoreCompileInputs.cache`).

--- APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fa807f4e9b98108f1f09bdfb9597a94cbbd2f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8e05db7efdd9ed3314c02bc8750d36c24f4266")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
